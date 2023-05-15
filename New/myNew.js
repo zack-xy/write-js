7165625f65b717a839eb39a41155093d7c7eccb7
zack zheng <zack_zhengxiyun@163.com>
2023-05-15 16:17:01 +0800

    no message

--- New/myNew.js
+++ New/myNew.js
@@ -5,7 +5,7 @@ function _new(ctor, ...args) {
   }
 
   let obj = new Object()
-  obj.__proto__ = Object.create(ctor.prototye)
+  obj.__proto__ = Object.create(ctor.prototype)
   let res = ctor.apply(obj, ...args)
 
   let isObject = typeof res === 'object' && typeof res !== null
@@ -13,3 +13,29 @@ function _new(ctor, ...args) {
   return isObject || isFunction ? res : obj
 
 }
+
+
+////////// 测试------代码
+
+function Parent(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Parent.prototype.sayName = function () {
+  console.log(this.name);
+};
+
+var str = "";
+
+// 失败测试
+var a = _new(str);
+// ctor must be a function
+
+// 成功
+var b = _new(Parent, "frank", 18);
+b.sayName(); // frank
+b instanceof Parent; // true
+b.hasOwnProperty("name"); // true
+b.hasOwnProperty("age"); // true
+b.hasOwnProperty("sayName"); // false

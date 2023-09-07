111272320093bab719450705dea7caa83082dcf1
zack zheng <zack_zhengxiyun@163.com>
2023-09-08 00:34:46 +0800

    update

--- underscore/_.js
+++ underscore/_.js
@@ -64,8 +64,74 @@
   //   console.log(2)
   // }
 
+  // 流式编程结束标志
   _.prototype.ending = function() {
     return this.wrapper
+  }
+
+  _.extend = function() {
+    var target = arguments[0]
+    var length = arguments.length
+    var i = 1
+    var options
+    for(;i<length;i++) {
+      if((options=arguments[i])!=null) {
+        for(key in options) {
+          target[key] = options[key]
+        }
+      }
+    }
+    return target
+  }
+
+  _.isObject = function(obj) {
+    return toString.call(obj) === '[object Object]'
+  }
+
+  var createReduce = function(dir) {
+    var reduce = function(obj, func, memo, init) {
+      var keys = !Array.isArray(obj) && Object.keys(obj)
+      var length = (keys || obj).length
+      var index = dir > 0 ? 0 : length - 1
+      if(!init) {
+        memo = obj[keys ? keys[index] : index]
+        index+=dir
+      }
+      for(;index>=0&&index<length;index+=dir) {
+        var currentKey = keys ? keys[index] : index
+        memo = func(memo, obj[currentKey], currentKey, obj)
+      }
+      return memo
+    } 
+    
+    return function(obj, func, memo) {
+      var init = arguments.length >= 3
+      return reduce(obj, func, memo, init)
+    }
+  }
+
+  _.reduce = createReduce(1)
+
+  // 浅拷贝
+  _.clone = function(obj) {
+    if(typeof obj !== 'object') return obj
+    return Array.isArray(obj) ? obj.slice() : _.extend({}, obj)
+  }
+
+  // 深拷贝
+  _.deepClone = function(obj) {
+    if(Array.isArray(obj)) {
+      return obj.map(function(item) {
+        return Array.isArray(item) || _.isObject(item) ? _.deepClone(item) : item
+      })
+    } else if(_.isObject(obj)) {
+      return _.reduce(obj, function(memo, value, key, target) {
+        memo[key] = Array.isArray(value) || _.isObject(value) ? _.deepClone(value) : value
+        return memo
+      }, {})
+    } else {
+      return obj
+    }
   }
 
   var beforeHook = function(arr, callback) {
